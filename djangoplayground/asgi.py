f08ccccbeaf6b876a1ffa872577e7756d77a7219
hashemzadeh <hashemzadeh@mahsan.co>
2024-12-11 22:56:01 +0330

    changed some skeleton

--- djangoplayground/asgi.py
+++ djangoplayground/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for djangoplaygroundproject project.
+ASGI config for djangoplayground project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoplaygroundproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoplayground.settings')
 
 application = get_asgi_application()
